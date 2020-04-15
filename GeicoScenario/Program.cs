11c7bbeb9131818c0d6acf0b85fbb2c37819feab
Erwin Tung <erwin@rollbar.com>
2020-04-15 15:15:47 -0700

    Test Code for Debugging Proxy Configuration Settings with the .NET SDK

---
+++ GeicoScenario/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Rollbar;
+
+namespace GeicoScenario
+{
+    public static class Program
+    {
+
+        private static void OnRollbarInternalEvent(object sender, RollbarEventArgs e)
+        {
+            Console.WriteLine(e.TraceAsString());
+
+            RollbarApiErrorEventArgs apiErrorEvent = e as RollbarApiErrorEventArgs;
+            if (apiErrorEvent != null)
+            {
+                //TODO: handle/report Rollbar API communication error event...
+                return;
+            }
+            CommunicationEventArgs commEvent = e as CommunicationEventArgs;
+            if (commEvent != null)
+            {
+                //TODO: handle/report Rollbar API communication event...
+                return;
+            }
+            CommunicationErrorEventArgs commErrorEvent = e as CommunicationErrorEventArgs;
+            if (commErrorEvent != null)
+            {
+                //TODO: handle/report basic communication error while attempting to reach Rollbar API service... 
+                return;
+            }
+            InternalErrorEventArgs internalErrorEvent = e as InternalErrorEventArgs;
+            if (internalErrorEvent != null)
+            {
+                //TODO: handle/report basic internal error while using the Rollbar Notifier... 
+                return;
+            }
+        }
+
+        public static void Main()
+        {
+            while (true)
+            {
+                const string postServerItemAccessToken = "INVALID";
+                // ProxyAddress = "10.128.0.5:3128"
+                RollbarLocator.RollbarInstance.Configure(
+                                                         new RollbarConfig(postServerItemAccessToken)
+                                                         {
+                                                             Environment = "development",
+                                                         }).InternalEvent += OnRollbarInternalEvent;
+                RollbarQueueController.Instance.InternalEvent += OnRollbarInternalEvent;
+                try
+                {
+                    int value = 1 / int.Parse("0");
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine("Error: You can't divide by zero.");
+                    try
+                    {
+                        RollbarLocator.RollbarInstance.Error(ex);
+                    }
+                    catch (System.Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
